5e0a9f67abae4054caa3b0bb9ba65b3de0ac932a
Andrew Everman <31459473+andreweverman@users.noreply.github.com>
2020-11-21 14:35:13 -0500

    jajajaja

--- dist/util/other_util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.get_command_files = void 0;
-const commands_path = "./src/commands";
-function get_command_files() {
-    return ['jaja'];
-}
-exports.get_command_files = get_command_files;
-//# sourceMappingURL=other_util.js.map
