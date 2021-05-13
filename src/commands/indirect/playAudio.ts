206af71ecf3abea320a88e52896d4d4b8693c403
andreweverman <andreweverman@gmail.com>
2021-05-13 02:30:41 -0400

    testing out on ubuntu:

--- src/commands/indirect/playAudio.ts
+++ src/commands/indirect/playAudio.ts
@@ -4,19 +4,22 @@
  * Plays a voice clip with params from mongo.
  * Bot then plays sound through discord
  * Plays from youtube.
- * 
- * 
+ *
+ *
  * @file   All logic for playing a youtube video through bot
  * @author Andrew Everman.
  * @since  16.7.2020
  */
 
-import { VoiceChannel } from 'discord.js'
+import { VoiceChannel, StreamDispatcher } from 'discord.js'
 import { ILinkDoc } from '../../db/models/guildModel'
 import { Player } from '../../db/controllers/guildController'
 import { sendToChannel, MessageChannel } from '../../util/messageUtil'
 import ytdl from 'ytdl-core'
 import { NumberConstants } from '../../util/constants'
+import { getClipFiles, clipsPath } from '../../util/fiileUtil'
+import fs from 'fs'
+import { Readable } from 'stream'
 
 const execute = async function (voiceChannel: VoiceChannel, clip: ILinkDoc, textChannel: MessageChannel) {
     try {
@@ -28,9 +31,29 @@ const execute = async function (voiceChannel: VoiceChannel, clip: ILinkDoc, text
 
         const connection = await voiceChannel.join()
 
-        const stream = ytdl(clip.link, { filter: 'audioonly' })
+        let playVal: Readable | string = ''
 
-        const dispatcher = connection.play(stream, { volume: clip.volume })
+        // todo actually get if they are premium or not
+        const premium = true
+        if (premium) {
+            const dlClip = findClip(clip)
+            if (dlClip) {
+                playVal = dlClip
+            }
+        }
+        if (playVal == '') {
+            playVal = ytdl(clip.link, { filter: 'audioonly' })
+            let piper = ytdl(clip.link, { filter: 'audioonly' })
+
+            if (premium) {
+                if (!fs.existsSync(clipsPath)) {
+                    fs.mkdirSync(clipsPath)
+                }
+                let filePath = `${clipsPath}/${justVEquals(clip.link)}.mp3`
+                piper.pipe(fs.createWriteStream(filePath))
+            }
+        }
+        const dispatcher = connection.play(playVal, { volume: clip.volume })
 
         dispatcher.on('error', (e) => {
             throw e
@@ -48,12 +71,34 @@ const execute = async function (voiceChannel: VoiceChannel, clip: ILinkDoc, text
                     if (player.lastStreamingTime != timestamp) {
                         connection.disconnect()
                     }
-                }, 15*NumberConstants.mins)
+                }, 15 * NumberConstants.mins)
             }
         })
     } catch (error) {
         throw error
     }
+}
+
+function findClip(clip: ILinkDoc): string | undefined {
+    const clipFiles = getClipFiles()
+    if (clipFiles.length > 0) {
+        const filePath = clipFiles.find((p) => p.end.includes(justVEquals(clip.link)))
+        return filePath?.path
+    }
+    return undefined
+}
+
+function justVEquals(ytLink: string) {
+    if (ytLink.includes('youtu.be')) {
+        return ytLink.substring(ytLink.lastIndexOf('/'))
+    } else {
+        let regex = /.*?v=(.*)\?*/
+        let res = regex.exec(ytLink)
+        if (res) {
+            return res[1]
+        }
+    }
+    return ''
 }
 
 export default execute
